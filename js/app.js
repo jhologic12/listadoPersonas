d36cc38beeb854edafd8efb3ded64af6d3f610a4
jhologic12 <jhonospino@gmail.com>
2022-11-07 15:15:28 -0500

    agregar personas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const personas = [
     new Persona ('Jhon' , 'Ospino'),
-    new Persona ('Alexander', 'Figueroa')
+    new Persona ('Alexander', 'Figueroa'),
 ];
 
 
@@ -15,5 +15,28 @@ function mostrarPersonas(){
    }
  
    document.getElementById('personas').innerHTML = texto;
+
+}
+
+
+function agregarPersonas(){
+
+    console.log('se presiono el boton agregarPersonas');
+    const formul = document.forms['formul'];
+    const nombre = formul['nombre'];
+    const apellido = formul['apellido'];
+
+
+    if (nombre.value != '' && apellido.value != ''){
+
+        const persona = new Persona (nombre.value , apellido.value);
+        personas.push(persona);
+        mostrarPersonas();
+        console.log(persona);
+    } 
+    else {
+        console.log("Debe ingresar los datos de nombre y apellido");
+    }
+
 
 }
