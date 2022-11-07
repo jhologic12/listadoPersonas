33cfd8ac99402dcd337b221df716e5c11b0e4d88
jhologic12 <jhonospino@gmail.com>
2022-11-07 14:09:36 -0500

    agregando funcionalidad a clase persona y app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("inicio de app.jss");
+const personas = [
+    new Persona ('Jhon' , 'Ospino'),
+    new Persona ('Alexander', 'Figueroa')
+];
+
+
+function mostrarPersonas(){
+    console.log('funcion mostrarPersonas Iniciada...');
+
+   let texto = '';
+
+   for (let persona of personas){
+
+        texto += `<li> ${persona.nombre} ${persona.apellido} </li>`;
+   }
+ 
+   document.getElementById('personas').innerHTML = texto;
+
+}
